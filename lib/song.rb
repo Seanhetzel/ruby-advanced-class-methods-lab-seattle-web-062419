d1986a0e7fcd793b59688666bcc90af47f69e0ca
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-01 06:36:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,13 @@ class Song
   def self.find_by_name(name)
     self.all.find {|song| song.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+    @@all.each do |song|
+    if @@all.include?(song.name)
+      Song.find_by_name(name)
+    else
+      Song.create_by_name(name)
+    end
+  end
 end
